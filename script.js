9d07417219e5c7dfe0b2897bb69ed01e6d513e54
Aadarsh <aadarshhanu23@gmail.com>
2022-08-23 20:09:47 +0530

    minor changes

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Competitive Coder", "Web Developer", "Socializer", "Gamer" ],
+        strings: ["Competitive Coder", "Web Developer", "App Developer", "ML Enthusiast", "Socializer",  "Gamer" ],
         typeSpeed: 80,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Competitive Coder", "Web Developer", "Socializer", "Gamer" ],
+        strings: ["Competitive Coder", "Web Developer", "App Developer", "ML Enthusiast", "Socializer",  "Gamer" ],
         typeSpeed: 80,
         backSpeed: 60,
         loop: true
